43f24c4e6af4ecf42dea07129b1e1981a43b2b75
Sunflower <99494687+anacarol1210@users.noreply.github.com>
2022-03-23 17:23:00 -0300

    Create pt2.ino

---
+++ Exercícios Arduíno/pt2.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000); 
+  digitalWrite(3, HIGH);
+  digitalWrite(2, LOW);
+  delay(1000); 
+  
+}
